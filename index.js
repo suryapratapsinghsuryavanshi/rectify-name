77fbacf90cc536c6d4cb15675cb340986274ed83
Suryapratap Singh <suryprtaps@gmail.com>
2022-01-22 02:03:29 +0530

    added initial functionality

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-// Get all file name in currnet dir
-const getAllFileName = () => {
-    return "dev";
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+// A method for checking this is an ignorable file or not.
+function ignoreFiles(file) {
+    return(
+		file.startsWith("package") ||
+		file.startsWith(".") ||
+		!file.includes(".") ||
+		file.includes(".git") ||
+		file.includes(".DS_Store") ||
+		file.includes(".idea") ||
+		file.includes(".vscode") ||
+        file.includes(".yml")
+	);
 }
 
-module.exports = {
-    getAllFileName
+// A method for separating file name and their extension.
+function getFileName(file) {
+	return file.split(".");
 }
+
+// final file name, rename method.
+
+function renameFile(fileNameArray) {
+	fileNameArray.map(file => {
+		// rename files
+		if(!ignoreFiles(file)) {
+			let [fName, fExt] = getFileName(file);
+			fs.renameSync(`./${fName}.${fExt}`, `./${fName.toLowerCase()}.${fExt}`);
+		}
+	});
+}
+
+// Getting all current directory files.
+fileNameArray = fs.readdirSync("./", { encoding: 'utf8' });
+
+renameFile(fileNameArray);
