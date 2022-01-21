442450879c54bb5b193896a47b90815730942db0
Suryapratap Singh <suryprtaps@gmail.com>
2022-01-21 09:10:49 +0530

    added type declaration

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+/**
+
+@exmaple
+```javascript
+const rectifyName = require('rectify-name');
+
+rectifyName("./", "camelCase");
+// All the file name convert to the camelCase.
+```
+*/
+type CaseType = "camelCase" | "snakeCase" | "kebabCase" | "pascalCase";
+export default function getAllFileName(path: string, caseType: CaseType): void;
