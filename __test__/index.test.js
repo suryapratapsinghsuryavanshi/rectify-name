98fafe81902f47d887c29a4aa1f0e6085afea0da
Suryapratap Singh <suryprtaps@gmail.com>
2022-07-14 10:35:21 +0530

    added auto detect case api and methods

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require("@jest/globals");
-const { ignoreFiles, getFileName } = require("./../index");
+const { ignoreFiles, getFileName, renameFile } = require("./../index");
 
 test("Test: ignoreFiles", () => {
     expect(ignoreFiles("package.json")).toBe(true);
@@ -7,4 +7,314 @@ test("Test: ignoreFiles", () => {
 
 test("Test: getFileName", () => {
     expect(getFileName("test.js")).toEqual(["test", "js"]);
+});
+
+test("Test: lower case", () => {
+	expect(renameFile({ dir: './', case: 'lower-case', caps: false }, [
+		'blog-1.txt',
+		'blog_2.txt',
+		'camelCase.txt',
+		'Capital Case.txt',
+		'CONSTANT_CASE.txt',
+		'details file me.js',
+		'PascalCase.txt'
+	])).toStrictEqual({
+		actualFiles: [
+			'blog-1.txt',
+			'blog_2.txt',
+			'camelCase.txt',
+			'Capital Case.txt',
+			'CONSTANT_CASE.txt',
+			'details file me.js',
+			'PascalCase.txt'
+		],
+		renamedFiles: [
+			'blog-1.txt',
+			'blog_2.txt',
+			'camelcase.txt',
+			'capital case.txt',
+			'constant_case.txt',
+			'details file me.js',
+			'pascalcase.txt'
+		]
+	});
+});
+
+test("Test: camelCae", () => {
+	expect(renameFile({ dir: './', case: 'camel-case', caps: false }, [
+		'blog-1.txt',
+		'blog_2.txt',
+		'camelCase.txt',
+		'Capital Case.txt',
+		'CONSTANT_CASE.txt',
+		'details file me.js',
+		'PascalCase.txt'
+	])).toStrictEqual({
+		actualFiles: [
+		  'blog-1.txt',
+		  'blog_2.txt',
+		  'camelCase.txt',
+		  'Capital Case.txt',
+		  'CONSTANT_CASE.txt',
+		  'details file me.js',
+		  'PascalCase.txt'
+		],
+		renamedFiles: [
+		  'blog1.txt',
+		  'blog2.txt',
+		  'camelCase.txt',
+		  'capitalCase.txt',
+		  'constantCase.txt',
+		  'detailsFileMe.js',
+		  'pascalCase.txt'
+		]
+	});
+});
+
+test("Test: CamelCase(caps)", () => {
+	expect(renameFile({ dir: './', case: 'camel-case', caps: true }, [
+		'blog-1.txt',
+		'blog_2.txt',
+		'camelCase.txt',
+		'Capital Case.txt',
+		'CONSTANT_CASE.txt',
+		'details file me.js',
+		'PascalCase.txt'
+	])).toStrictEqual({
+		actualFiles: [
+		  'blog-1.txt',
+		  'blog_2.txt',
+		  'camelCase.txt',
+		  'Capital Case.txt',
+		  'CONSTANT_CASE.txt',
+		  'details file me.js',
+		  'PascalCase.txt'
+		],
+		renamedFiles: [
+		  'Blog1.txt',
+		  'Blog2.txt',
+		  'CamelCase.txt',
+		  'CapitalCase.txt',
+		  'ConstantCase.txt',
+		  'DetailsFileMe.js',
+		  'PascalCase.txt'
+		]
+	});
+});
+
+test("Test: Capital Case", () => {
+	expect(renameFile({ dir: './', case: 'capital-case', caps: false }, [
+		'blog-1.txt',
+		'blog_2.txt',
+		'camelCase.txt',
+		'Capital Case.txt',
+		'CONSTANT_CASE.txt',
+		'details file me.js',
+		'PascalCase.txt'
+	])).toStrictEqual({
+		actualFiles: [
+		  'blog-1.txt',
+		  'blog_2.txt',
+		  'camelCase.txt',
+		  'Capital Case.txt',
+		  'CONSTANT_CASE.txt',
+		  'details file me.js',
+		  'PascalCase.txt'
+		],
+		renamedFiles: [
+		  'Blog 1.txt',
+		  'Blog 2.txt',
+		  'Camel Case.txt',
+		  'Capital Case.txt',
+		  'Constant Case.txt',
+		  'Details File Me.js',
+		  'Pascal Case.txt'
+		]
+	});
+});
+
+test("Test: CONSTENT_CASE", () => {
+	expect(renameFile({ dir: './', case: 'constant-case', caps: false }, [
+		'blog-1.txt',
+		'blog_2.txt',
+		'camelCase.txt',
+		'Capital Case.txt',
+		'CONSTANT_CASE.txt',
+		'details file me.js',
+		'PascalCase.txt'
+	])).toStrictEqual({
+		actualFiles: [
+			'blog-1.txt',
+			'blog_2.txt',
+			'camelCase.txt',
+			'Capital Case.txt',
+			'CONSTANT_CASE.txt',
+			'details file me.js',
+			'PascalCase.txt'
+		],
+		renamedFiles: [
+			'BLOG_1.txt',
+			'BLOG_2.txt',
+			'CAMEL_CASE.txt',
+			'CAPITAL_CASE.txt',
+			'CONSTANT_CASE.txt',
+			'DETAILS_FILE_ME.js',
+			'PASCAL_CASE.txt'
+		]
+	});
+});
+
+test("Test: kebab-case", () => {
+	expect(renameFile({ dir: './', case: 'kebab-case', caps: false }, [
+		'blog-1.txt',
+		'blog_2.txt',
+		'camelCase.txt',
+		'Capital Case.txt',
+		'CONSTANT_CASE.txt',
+		'details file me.js',
+		'PascalCase.txt'
+	])).toStrictEqual({
+		actualFiles: [
+			'blog-1.txt',
+			'blog_2.txt',
+			'camelCase.txt',
+			'Capital Case.txt',
+			'CONSTANT_CASE.txt',
+			'details file me.js',
+			'PascalCase.txt'
+		],
+		renamedFiles: [
+			'blog-1.txt',
+			'blog-2.txt',
+			'camel-case.txt',
+			'capital-case.txt',
+			'constant-case.txt',
+			'details-file-me.js',
+			'pascal-case.txt'
+		]
+	});
+});
+
+test("Test: Kebab-Case(caps)", () => {
+	expect(renameFile({ dir: './', case: 'kebab-case', caps: true }, [
+		'blog-1.txt',
+		'blog_2.txt',
+		'camelCase.txt',
+		'Capital Case.txt',
+		'CONSTANT_CASE.txt',
+		'details file me.js',
+		'PascalCase.txt'
+	])).toStrictEqual({
+		actualFiles: [
+			'blog-1.txt',
+			'blog_2.txt',
+			'camelCase.txt',
+			'Capital Case.txt',
+			'CONSTANT_CASE.txt',
+			'details file me.js',
+			'PascalCase.txt'
+		],
+		renamedFiles: [
+			'Blog-1.txt',
+			'Blog-2.txt',
+			'Camel-Case.txt',
+			'Capital-Case.txt',
+			'Constant-Case.txt',
+			'Details-File-Me.js',
+			'Pascal-Case.txt'
+		]
+	});
+});
+
+test("Test: PascalCase", () => {
+	expect(renameFile({ dir: './', case: 'pascal-case', caps: false }, [
+		'blog-1.txt',
+		'blog_2.txt',
+		'camelCase.txt',
+		'Capital Case.txt',
+		'CONSTANT_CASE.txt',
+		'details file me.js',
+		'PascalCase.txt'
+	])).toStrictEqual({
+		actualFiles: [
+			'blog-1.txt',
+			'blog_2.txt',
+			'camelCase.txt',
+			'Capital Case.txt',
+			'CONSTANT_CASE.txt',
+			'details file me.js',
+			'PascalCase.txt'
+		],
+		renamedFiles: [
+			'Blog1.txt',
+			'Blog2.txt',
+			'CamelCase.txt',
+			'CapitalCase.txt',
+			'ConstantCase.txt',
+			'DetailsFileMe.js',
+			'PascalCase.txt'
+		]
+	});
+});
+
+test("Test: snake_case", () => {
+	expect(renameFile({ dir: './', case: 'snake-case', caps: false }, [
+		'blog-1.txt',
+		'blog_2.txt',
+		'camelCase.txt',
+		'Capital Case.txt',
+		'CONSTANT_CASE.txt',
+		'details file me.js',
+		'PascalCase.txt'
+	])).toStrictEqual({
+		actualFiles: [
+			'blog-1.txt',
+			'blog_2.txt',
+			'camelCase.txt',
+			'Capital Case.txt',
+			'CONSTANT_CASE.txt',
+			'details file me.js',
+			'PascalCase.txt'
+		],
+		renamedFiles: [
+			'blog_1.txt',
+			'blog_2.txt',
+			'camel_case.txt',
+			'capital_case.txt',
+			'constant_case.txt',
+			'details_file_me.js',
+			'pascal_case.txt'
+		]
+	});
+});
+
+test("Test: Snake_Case(caps)", () => {
+	expect(renameFile({ dir: './', case: 'snake-case', caps: true }, [
+		'blog-1.txt',
+		'blog_2.txt',
+		'camelCase.txt',
+		'Capital Case.txt',
+		'CONSTANT_CASE.txt',
+		'details file me.js',
+		'PascalCase.txt'
+	])).toStrictEqual({
+		actualFiles: [
+			'blog-1.txt',
+			'blog_2.txt',
+			'camelCase.txt',
+			'Capital Case.txt',
+			'CONSTANT_CASE.txt',
+			'details file me.js',
+			'PascalCase.txt'
+		],
+		renamedFiles: [
+			'Blog_1.txt',
+			'Blog_2.txt',
+			'Camel_Case.txt',
+			'Capital_Case.txt',
+			'Constant_Case.txt',
+			'Details_File_Me.js',
+			'Pascal_Case.txt'
+		]
+	});
 });
--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const fs = require("fs");
 const pkgJson = require("./package.json");
 const caseShift = require("case-shift");
 
+// Global CONSTANTS
+const LOWER_CASE = "lower-case";		// lowercase
+const CAMEL_CASE = "camel-case"; 		// camelCase
+const CAPITAL_CASE = "capital-case";	// Capital Case or Title Case
+const CONSTANT_CASE = "constant-case";	// CONSTANT_CASE
+const KEBAB_CASE = "kebab-case";		// kebab-case
+const PASCAL_CASE = "pascal-case";		// PascalCase
+const SNAKE_CASE = "snake-case";		// sanke_case
+
 // A method for checking this is an ignorable file or not.
 function ignoreFiles(file) {
 	return (
@@ -69,6 +78,57 @@ this package also recognizes some non-rectifiable files like .npmrc, and skips t
 `);
 }
 
+// detect which type of case use in files name.
+function caseDetect(case_type) {
+	// ![\s_-], regex check all the cases have no -, _ or no space.
+	if(!case_type.match(/[\s_-]/g)) {
+		let [ lowerToUpperTrans, firstLaterSmall ] = [case_type.match(/([a-z][A-Z])/g), case_type.match(/^([a-z])/g)];
+		if(lowerToUpperTrans !== null && firstLaterSmall !== null) {
+			if((lowerToUpperTrans.length >= 1) && (firstLaterSmall.length >= 1)) {
+				return "camel-case";
+			}
+		}
+
+		[ lowerToUpperTrans, firstLaterCap ] = [case_type.match(/([a-z][A-Z])/g), case_type.match(/^([A-Z])/g)];
+		if(lowerToUpperTrans !== null && firstLaterCap !== null) {
+			if((lowerToUpperTrans.length >= 1) && (firstLaterCap.length >= 1)) {
+				return "pascal-case";
+			}
+		}
+	}
+
+	// [\s_-], regex check all the cases have -, _ or space.
+	if(case_type.match(/[\s_-]/g)) {
+		let [ separatorSymbol, caseTransition ] = [ case_type.match(/([_])/g), case_type.toUpperCase() ];
+		if(separatorSymbol !== null) {
+			if(separatorSymbol.length >= 1 && case_type === caseTransition) {
+				return "constant-case";
+			}else if(separatorSymbol.length >= 1 && case_type === case_type.toLowerCase()) {
+				return "snake-case";
+			}
+		}
+
+		[ separatorSymbol, caseTransition ] = [ case_type.match(/([-])/g), case_type.toLowerCase() ];
+		if(separatorSymbol !== null) {
+			if(separatorSymbol.length >= 1 && case_type === caseTransition) {
+				return "kebab-case";
+			}
+		}
+
+		[ separatorSymbol, caseTransition ] = [ case_type.match(/([\s])/g), case_type.replace(/\w\S*/g, (str) => str.charAt(0).toUpperCase() + str.substr(1).toLowerCase()) ];
+		if(separatorSymbol !== null) {
+			if(separatorSymbol.length >= 1 && case_type === caseTransition) {
+				return "capital-case";
+			}
+		}
+
+		if(case_type === case_type.toLowerCase()) {
+			return "lower-case";
+		}
+	}
+
+}
+
 const finalConfig = {
 	dir: "./",
 	case: "lower-case",
@@ -76,13 +136,13 @@ const finalConfig = {
 }
 
 const caseType = [
-	"lower-case",
-	"camel-case",
-	"capital-case",
-	"constant-case",
-	"kebab-case",
-	"pascal-case",
-	"snake-case"
+	"lower-case",	// lowercase
+	"camel-case", 	// camelCase
+	"capital-case",	// Capital Case or Title Case
+	"constant-case",// CONSTANT_CASE
+	"kebab-case",	// kebab-case
+	"pascal-case",	// PascalCase
+	"snake-case"	// sanke_case
 ]
 
 function processArgs() {
@@ -155,42 +215,209 @@ function processArgs() {
 				default:
 					console.log(help());
 			}
+
+			if(args.includes("--caps")) {
+				finalConfig.caps = true;
+			}
+
+			// Getting all current directory files.
+			let fileNameArray = fs.readdirSync(finalConfig.dir, { encoding: "utf8" });
+			// rename the file according to the above parameters.
+			renameFile(finalConfig, fileNameArray);
+
+		}else {
+			console.log(help());
 		}
 	}catch(e) {
 		console.log("Error:", e);
 		console.log(`Check all options properly!, use rectify-name -h\n`);
 		process.exit(1);
 	}
-
-	// rename the file according to the above parameters.
-	renameFile(finalConfig)
 }
 
 // final file name, rename method.
-function renameFile(options) {
+function renameFile(options, fileNameArray) {
 
-	// Getting all current directory files.
-	let fileNameArray = fs.readdirSync(options.dir, { encoding: "utf8" });
-
-	let rectifiedFileNameArray = [];
+	let rectifiedFileNames = {
+		actualFiles: [],
+		renamedFiles: []
+	}
 	fileNameArray.map(file => {
 		// rename files
 		if (!ignoreFiles(file)) {
 			let [fName, fExt] = getFileName(file);
+			let nFName = fName;
+			let file_case_type = caseDetect(nFName);
 			switch(options.case) {
 				// check what type of case-type file name is used and convert it into the given type, need auto-detecting case-type API.
+				case CAMEL_CASE:
+					switch(file_case_type) {
+						case CAPITAL_CASE:
+							nFName = caseShift.capitalToCamel(nFName, options.caps);
+							break;
+						case CONSTANT_CASE:
+							nFName = caseShift.constantToCamel(nFName, options.caps);
+							break;
+						case KEBAB_CASE:
+							nFName = caseShift.kebabToCamel(nFName, options.caps);
+							break;
+						case PASCAL_CASE:
+							nFName = caseShift.pascalToCamel(nFName, options.caps);
+							break;
+						case SNAKE_CASE:
+							nFName = caseShift.snakeToCamel(nFName, options.caps);
+							break;
+						case CAMEL_CASE:
+							nFName = caseShift.pascalToCamel(nFName[0].toUpperCase() + nFName.slice(1), options.caps);
+							break;
+						case LOWER_CASE:
+							nFName = caseShift.constantToCamel(nFName.replace(/ /g, "_"), options.caps);
+							break;
+					}
+					break;
+				case CAPITAL_CASE:
+					switch(file_case_type) {
+						case CAMEL_CASE:
+							nFName = caseShift.camelToCapital(nFName);
+							break;
+						case CONSTANT_CASE:
+							nFName = caseShift.constantToCapital(nFName);
+							break;
+						case KEBAB_CASE:
+							nFName = caseShift.kebabToCapital(nFName);
+							break;
+						case PASCAL_CASE:
+							nFName = caseShift.pascalToCapital(nFName);
+							break;
+						case SNAKE_CASE:
+							nFName = caseShift.snakeToCapital(nFName);
+							break;
+						case CAPITAL_CASE:
+							nFName = nFName;
+							break;
+						case LOWER_CASE:
+							nFName = caseShift.constantToCapital(nFName.replace(/ /g, "_"));
+							break;
+					}
+					break;
+				case CONSTANT_CASE:
+					switch(file_case_type) {
+						case CAMEL_CASE:
+							nFName = caseShift.camelToConstant(nFName);
+							break;
+						case CAPITAL_CASE:
+							nFName = caseShift.capitalToConstant(nFName);
+							break;
+						case KEBAB_CASE:
+							nFName = caseShift.kebabToConstant(nFName);
+							break;
+						case PASCAL_CASE:
+							nFName = caseShift.pascalToConstant(nFName);
+							break;
+						case SNAKE_CASE:
+							nFName = caseShift.snakeToConstant(nFName);
+							break;
+						case CONSTANT_CASE:
+							nFName = nFName;
+							break;
+						case LOWER_CASE:
+							nFName = nFName.replace(/ /g, "_").toUpperCase();
+							break;
+					}
+					break;
+				case KEBAB_CASE:
+					switch(file_case_type) {
+						case CAMEL_CASE:
+							nFName = caseShift.camelToKebab(nFName, options.caps);
+							break;
+						case CAPITAL_CASE:
+							nFName = caseShift.capitalToKebab(nFName, options.caps);
+							break;
+						case CONSTANT_CASE:
+							nFName = caseShift.constantToKebab(nFName, options.caps);
+							break;
+						case PASCAL_CASE:
+							nFName = caseShift.pascalToKebab(nFName, options.caps);
+							break;
+						case SNAKE_CASE:
+							nFName = caseShift.snakeToKebab(nFName, options.caps);
+							break;
+						case KEBAB_CASE:
+							nFName = caseShift.constantToKebab(nFName, options.caps);
+							break;
+						case LOWER_CASE:
+							nFName = caseShift.constantToKebab(nFName.replace(/ /g, "_"), options.caps);
+							break;
+					}
+					break;
+				case PASCAL_CASE:
+					switch(file_case_type) {
+						case CAMEL_CASE:
+							nFName = caseShift.camelToPascal(nFName);
+							break;
+						case CAPITAL_CASE:
+							nFName = caseShift.capitalToPascal(nFName);
+							break;
+						case CONSTANT_CASE:
+							nFName = caseShift.constantToPascal(nFName);
+							break;
+						case KEBAB_CASE:
+							nFName = caseShift.kebabToPascal(nFName);
+							break;
+						case SNAKE_CASE:
+							nFName = caseShift.snakeToPascal(nFName);
+							break;
+						case PASCAL_CASE:
+							nFName = nFName;
+							break;
+						case LOWER_CASE:
+							nFName = caseShift.constantToPascal(nFName.replace(/ /g, "_"));
+							break;
+					}
+					break;
+				case SNAKE_CASE:
+					switch(file_case_type) {
+						case CAMEL_CASE:
+							nFName = caseShift.camelToSnake(nFName, options.caps);
+							break;
+						case CAPITAL_CASE:
+							nFName = caseShift.capitalToSnake(nFName, options.caps);
+							break;
+						case CONSTANT_CASE:
+							nFName = caseShift.constantToSnake(nFName, options.caps);
+							break;
+						case KEBAB_CASE:
+							nFName = caseShift.kebabToSnake(nFName, options.caps);
+							break;
+						case PASCAL_CASE:
+							nFName = caseShift.pascalToSnake(nFName, options.caps);
+							break;
+						case SNAKE_CASE:
+							nFName = caseShift.constantToSnake(nFName, options.caps);
+							break;
+						case LOWER_CASE:
+							nFName = caseShift.constantToSnake(nFName.replace(/ /g, "_"), options.caps);
+							break;
+					}
+					break;
+				case LOWER_CASE:
+					nFName = nFName.toLowerCase();
+					break;
 			}
+			rectifiedFileNames.actualFiles.push(`${fName}.${fExt}`);
+			rectifiedFileNames.renamedFiles.push(`${nFName}.${fExt}`);
 		}
 	});
-	return rectifiedFileNameArray;
+	console.log(rectifiedFileNames);
+	return rectifiedFileNames;
 }
 
 // pass all the filenames to the renameFile method they take care about what file we will ignore or what are eligible to rectify.
 // renameFile(fileNameArray);
-processArgs()
+processArgs();
 
 module.exports = {
 	ignoreFiles,
 	getFileName,
-	processArgs
+	renameFile
 }
